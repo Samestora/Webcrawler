ee9f2f956e0d416cec64c73d68c947f65a35e8bc
Samestora <putrantosurya38@gmail.com>
2023-07-05 11:58:58 +0700

    new file:   xkcd/.gitignore 	new file:   xkcd/Downloader.py 	new file:   xkcd/README 	new file:   xkcd/Scrapper.py

---
+++ xkcd/Scrapper.py
@@ -0,0 +1,25 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+def Scrape():
+	# Staging
+	user = str(input('Type the desired page number : '))
+	url = 'https://xkcd.com/' + user + '/'
+	user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
+	header = {'User-Agent' : user_agent} 
+
+	#Request the URL and open it
+	requesting = urllib.request.Request(url, None ,header)
+
+	with urllib.request.urlopen(requesting) as resp:
+		read = resp.read()
+
+	# Parse the HTML
+	soup_read = BeautifulSoup(read, 'html.parser')
+	url_list = []
+
+	# Find all the URLs to get the desired URL
+	for link in soup_read.find_all('a'):
+		url_list.append(link.get('href'))
+
+	return url_list[26] # This is the desired URL
