f34ad2e5596ed1450f43e23ebb6adae84f3504be
Samestora <putrantosurya38@gmail.com>
2023-07-07 14:08:36 +0700

    delete this

--- xkcd/Downloader.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-import Scrapper
-
-def IMG_Download(url, filepath):
-	urllib.request.urlretrieve(url, filepath)
-	
-IMG_Download(Scrapper.Scrape(), 'Downloads/' + str(input('filename? : ')) + '.jpg')
